a930b0916519de5ec83dd5c430f10f01cc180bc5
Chris Adderley <cadderley@gmail.com>
2020-04-23 15:33:23 -0700

    Glowy test

---
+++ Source/Restock/ModuleRestockLinkedColorChanger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Restock
+{
+  public class ModuleRestockLinkedColorChanger : ModuleColorChanger
+  {
+
+    // The module to link this ColorChanger to
+    [KSPField] public string parentModuleID = "moduleName";
+
+    public ModuleColorChanger parentModuleColorChanger;
+
+    public override void Start()
+    {
+      base.Start();
+
+      if (HighLogic.LoadedSceneIsFlight || HighLogic.LoadedSceneIsEditor)
+      {
+        
+        foreach (ModuleColorChanger mcc in part.GetComponents<ModuleColorChanger>())
+        {
+          Debug.Log(mcc.moduleID);
+          if (mcc.moduleID == parentModuleID)
+          {
+            parentModuleColorChanger = mcc;
+          }
+        }
+      }
+    }
+
+    public override void FixedUpdate()
+    {
+      base.FixedUpdate();
+      if (!parentModuleColorChanger)
+        return;
+      
+      SetScalar(parentModuleColorChanger.GetScalar);
+      
+    }
+  }
+}
